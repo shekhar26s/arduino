60cffbfa5a01cfa9089b1914ff56195f0e43d96a
Luis Ignacio Díaz Martínez <luigdima@gmail.com>
2013-07-09 00:29:37 +0200

    Novelties!

---
+++ SketchBook/Serial/Info2lcd_12864/Info2lcd_12864.ino
@@ -0,0 +1,56 @@
+/*
+ * Drive an ST7920 LCD module via the serial interface.
+ *
+ * luigdima.name
+ *
+ * Configuration
+ * LCD | 328/Arduino
+ * ------------------------
+ * GND | GND
+ * VCC | 5V
+ * E   | SCLK == PB5 == D13
+ * RW  | MOSI == PB3 == D11
+ * RST | PB0 == D8
+ * RS  | 5V
+ * A   | 390R to 5V
+ * K   | GND
+ *
+ */
+
+#include <U8glib.h>
+
+U8GLIB_ST7920_128X64 u8g(13, 11, 8, U8G_PIN_NONE);
+
+void setup() {    
+  Serial.begin(115200);
+}  
+
+void draw(void) {
+  u8g.setFont(u8g_font_5x8);
+  
+  u8g.drawStr(42, 6, "CPU");   
+  u8g.drawStr(110, 6, "28%");   
+  
+  u8g.drawStr(42, 15, "RAM");   
+  u8g.drawStr(110, 15, "85%");     
+  
+  u8g.drawStr(42, 24, "TMP");   
+  u8g.drawStr(110, 24, "73%");     
+    
+  
+  u8g.setFont(u8g_font_freedoomr25n);
+  u8g.drawStr(0, 26, "68");   
+  
+  u8g.drawBox(1,30,15,5);
+  u8g.drawFrame(1,30,36,5);
+}
+
+void loop(void) {
+  u8g.firstPage();  
+  
+  //Aqui envio
+  
+  do {
+    draw();
+  } while( u8g.nextPage() );
+}
